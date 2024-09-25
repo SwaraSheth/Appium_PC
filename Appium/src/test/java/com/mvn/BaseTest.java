3c23690e74b2b88f48fdca0db5b76fc05815f07e
Admin <Admin@DESKTOP-FJRQFLH>
2024-09-25 17:25:11 +0530

    New

--- Appium/src/test/java/com/mvn/BaseTest.java
+++ Appium/src/test/java/com/mvn/BaseTest.java
@@ -4,8 +4,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
@@ -17,13 +22,29 @@ public AndroidDriver driver;
 public void before() throws MalformedURLException, InterruptedException {
 	UiAutomator2Options options=new UiAutomator2Options();
 	options.setDeviceName("Swara");
-//	options.setApp("D:\\Appium\\apk file\\ApiDemos-debug.apk");
-	options.setApp("D:\\Appium\\apk file\\General-Store.apk");
+	options.setApp("D:\\Appium\\apk file\\ApiDemos-debug.apk");
+//	options.setApp("D:\\Appium\\apk file\\General-Store.apk");
 	 driver=new AndroidDriver
 			(new URL("http://127.0.0.1:4723/"),options);
 	 //implicit wait
 	 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	Thread.sleep(2000);
+}
+
+//Drag & Drop
+public void dragndrop(WebElement source) {
+	((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of(
+		    "elementId", ((RemoteWebElement) source).getId(),
+		    "endX", 719,
+		    "endY", 960));
+}
+
+//Long Press
+public void longPressAction(WebElement ele)
+{
+	((JavascriptExecutor)driver).executeScript("mobile: longClickGesture",
+			ImmutableMap.of("elementId",((RemoteWebElement)ele).getId(),
+					"duration",2000));
 }
 
 @AfterTest
---
+++ Appium/src/test/java/com/mvn/LongPress.java
@@ -0,0 +1,34 @@
+package com.mvn;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+
+public class LongPress extends BaseTest {
+
+	@Test
+	public void press() throws InterruptedException {
+		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector())" + ".scrollIntoView(text(\"Views\"));"))
+		.click();
+		Thread.sleep(2000);
+		
+		driver.findElement(AppiumBy.accessibilityId("Expandable Lists")).click();
+		Thread.sleep(3000);
+		
+		driver.findElement(By.id("android:id/text1")).click();
+		Thread.sleep(3000);
+		
+//		//class_Value[@text='text_value']
+		WebElement e1= driver.findElement(By.xpath("//android.widget.TextView[@text='People Names']"));
+		longPressAction(e1);
+		Thread.sleep(2000);
+		
+		
+		String msg=driver.findElement(By.id("android:id/title")).getText();
+		assertEquals(msg, "Sample menu");
+	}
+}
